10e2cd8816f60832bb753f7fbb6b063348c28c10
Radhey Patel <46446130+Arthur-272@users.noreply.github.com>
2020-09-22 19:35:12 +0530

    Made the first letter of user capital in generateBat.py

--- generateBat.py
+++ generateBat.py
@@ -2,6 +2,7 @@
 
 def generateBat(user):
 	f = open('windowsWallpaper.bat','wt')
+	user = user[0].capitalize() + str(user[1:])
 	f.write('reg add "HKEY_CURRENT_USER\\Control Panel\\Desktop" /v Wallpaper /t REG_SZ /d C:\\Users\\'+user+'\\Desktop\\wallpaper.bmp /f \n')
 	f.write('RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters')
 	f.close()
