8c77da00bb60bd7ef57b5e6e59209076ca5e0d14
Radhey Patel <46446130+Arthur-272@users.noreply.github.com>
2020-09-25 02:34:11 +0530

    Nothing new

--- color.py
+++
@@ -1,8 +0,0 @@
-from colorama import init, Fore, Back, Style
-from termcolor import colored
-
-init()
-
-def print(string):
-	print(colored(Fore.RED + string))
-print("Radhey")
--- test.py
+++
@@ -1,3 +0,0 @@
-f = open('/home/kali/Desktop/test.txt','wb')
-f.write("Hello")
-f.close()
