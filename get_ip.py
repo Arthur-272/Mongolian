c4c77c6de22c5eeaa3c90e8ab5102f2b3db59f68
Radhey Patel <46446130+Arthur-272@users.noreply.github.com>
2020-09-25 03:25:53 +0530

    Added the functionality of host_os in getIP

--- get_ip.py
+++ get_ip.py
@@ -6,7 +6,7 @@ import re
 
 flag = False
 
-def getIP():
+def returnIP():
     global ip, flag
     while True:
         if flag:
@@ -25,16 +25,28 @@ def task(n, cmd):
         ip.append(temp)
     if n == 254:
         flag = True
-cmd = 'sudo ifconfig'
-cmd = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE).stdout.read()
-temp = re.findall('inet (.+)  netmask', str(cmd))
-temp = temp[0].split('.')
-network = temp[0] + "."+ temp[1] + "." + temp[2] + "."
-ip = []
-for i in range(1,255):
-    t = threading.Thread(name = i, target = task, args = (i, network))
-    t.daemon = False
-    t.start()
 
-
-
+def getIP(host_os):
+    if host_os == 'Linux':
+        cmd = 'sudo ifconfig'
+        cmd = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE).stdout.read()
+        temp = re.findall('inet (.+)  netmask', str(cmd))
+        temp = temp[0].split('.')
+        network = temp[0] + "."+ temp[1] + "." + temp[2] + "."
+        ip = []
+        for i in range(1,255):
+            t = threading.Thread(name = i, target = task, args = (i, network))
+            t.daemon = False
+            t.start()
+    elif host_os == 'Windows':
+        cmd = 'ipconfig':
+        cmd = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE).stdout.read()
+        cmd = re.findall('IPv4 Address .+ : (.+)')
+        temp = temp[0].split('.')
+        network = temp[0] + '.' + temp[1] + '.' + temp[2] + '.'
+        ip = []
+        for i in range(1,255):
+            t = threading.Thread(name = i, target = task, args = (i, network))
+            t.daemon = False
+            t.start()
+    return returnIP()
